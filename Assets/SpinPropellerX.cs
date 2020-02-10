e4b990e5f2db66622d7eecfd5c3432bcf8301193
karenharen <karenharenschool@gmail.com>
2020-02-10 11:23:19 -0800

    didn't use the same name as the script in the prompt. Updated that.

--- Assets/SpinPropellerX.cs
+++ Assets/SpinPropellerX.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotateObject : MonoBehaviour
+public class SpinPropellerX : MonoBehaviour
 {
     public float rotationSpeed = 25;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
